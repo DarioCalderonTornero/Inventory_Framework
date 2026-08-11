f30ae7b9255f4d93418d71ab26483915ec2185d9
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2026-08-11 20:44:33 +0200

    Initial commit

---
+++ Source/Inventory_Framework/Items/ItemDefinition.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_FRAMEWORK_API UItemDefinition : public UObject
+{
+	GENERATED_BODY()
+	 
+};
